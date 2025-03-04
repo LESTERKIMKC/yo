968f3df539db5a0895afe60b067bde4aab750117
LESTERKIMKC <yearprium@gmail.com>
2025-03-04 15:41:58 +0530

    soul.py

--- soul.py
+++ soul.py
@@ -13,11 +13,11 @@ from threading import Thread
 
 loop = asyncio.get_event_loop()
 
-TOKEN = '7323'
+TOKEN = '7449545940:AAHbhjxBRYueHbWH2PQvT4ywoSXFRbHeGZU'
 MONGO_URI = 'mongodb+srv://rishi:ipxkingyt@rishiv.ncljp.mongodb.net/?retryWrites=true&w=majority&appName=rishiv'
-FORWARD_CHANNEL_ID = -1002234
-CHANNEL_ID = -10022342
-error_channel_id = -10022
+FORWARD_CHANNEL_ID = -1002196043702
+CHANNEL_ID = -1002196043702
+error_channel_id = -1002196043702
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
